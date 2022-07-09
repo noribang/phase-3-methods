e40186a38ce3c8bf0524883859560709a040007d
hoonkim <hoonk9387@gmail.com>
2022-07-09 16:42:39 -0500

    Completed assignment. All 5 Methods now pass rspec tests.

--- methods.rb
+++ methods.rb
@@ -8,3 +8,21 @@ end
 def greet(name)
     puts "Hello, #{name}!"
 end
+# 3
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+# 4
+def add(num1, num2)
+    num1 + num2
+end
+# 5
+def halve(num)
+    # If argument is not an Integer
+    # return nil
+    if num.class != Integer
+        return nil
+    end
+    # Else return Integer argument divided by 2.
+    num / 2
+end
